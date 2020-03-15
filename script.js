c54fe1d2c9a56b5492eaad36c8f8d5585d29ef46
Ivan Ivanov <ivanov-i.s@mail.ru>
2020-03-15 12:03:45 +0300

    feat: add scripts to portfolio tabs and images

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+/*////////////HEADER////////////*/
+
+let navigationList = document.querySelectorAll("#navElement");
+let anchorList = document.querySelectorAll(".anchor");
+
+document.addEventListener('scroll',function(){    
+    for (let i=0; i<anchorList.length; i++){
+        if (window.pageYOffset>=anchorList[i].offsetTop){
+            navigationList.forEach(element => element.classList.remove('currentNav'));
+            navigationList[i].classList.add('currentNav');
+        }
+    }
+});
+
+
+/*////////////SLIDER////////////*/
+
 document.querySelector("#horizPhoneButton").addEventListener('click',function(){
     let screen = document.querySelector("#horizPhoneScreen");
     if (screen.classList.value.indexOf('turnOff')<0) screen.classList.add('turnOff');
@@ -10,13 +27,57 @@ document.querySelector("#vertPhoneButton").addEventListener('click',function(){
     else screen.classList.remove('turnOff');
 });
 
-let navigationList = document.querySelectorAll("#navElement");
-let anchorList = document.querySelectorAll(".anchor");
-document.addEventListener('scroll',function(){    
-    for (let i=0; i<anchorList.length; i++){
-        if (window.pageYOffset>=anchorList[i].offsetTop){
-            navigationList.forEach(element => element.classList.remove('currentNav'));
-            navigationList[i].classList.add('currentNav');
-        }
+
+/*////////////PORTFOLIO////////////*/
+
+let defaultGallery = document.querySelector("#portfolioGallery").innerHTML;
+let portfolioButtons = document.querySelector("#portfolioNavigation").querySelectorAll('button');
+
+document.querySelector("#portfolioButoonAll").addEventListener('click',function(){
+    let galleryItems = document.querySelectorAll("#portfolioImage");
+    let current = galleryItems.length-1;
+    while (current >= 0){
+        let randomFirst = Math.floor(Math.random() * current);
+        let buffer = galleryItems[randomFirst].innerHTML;
+        galleryItems[randomFirst].innerHTML = galleryItems[current].innerHTML;
+        galleryItems[current].innerHTML = buffer;
+        current--;
     }
+    portfolioButtons.forEach(element => element.classList.remove('currentTab'));
+    document.querySelector("#portfolioButoonAll").classList.add('currentTab');
+});
+
+document.querySelector("#portfolioButoonWebDesign").addEventListener('click',function(){
+    let galleryItems = document.querySelectorAll("#portfolioImage");
+    let prevImg = galleryItems[0].innerHTML;
+    for (let i=galleryItems.length-1; i>=0; i--){
+        let currentImg = galleryItems[i].innerHTML;
+        galleryItems[i].innerHTML = prevImg;
+        prevImg = currentImg;
+    }
+    portfolioButtons.forEach(element => element.classList.remove('currentTab'));
+    document.querySelector("#portfolioButoonWebDesign").classList.add('currentTab');
+});
+
+document.querySelector("#portfolioButoonGraphicDesign").addEventListener('click',function(){
+    let galleryItems = document.querySelectorAll("#portfolioImage");
+    let nextImg = galleryItems[galleryItems.length-1].innerHTML;
+    for (let i=0; i<galleryItems.length; i++){
+        let currentImg = galleryItems[i].innerHTML;
+        galleryItems[i].innerHTML = nextImg;
+        nextImg = currentImg;
+    }
+    portfolioButtons.forEach(element => element.classList.remove('currentTab'));
+    document.querySelector("#portfolioButoonGraphicDesign").classList.add('currentTab');
+});
+
+document.querySelector("#portfolioButoonArtWork").addEventListener('click',function(){
+    document.querySelector("#portfolioGallery").innerHTML=defaultGallery;
+    portfolioButtons.forEach(element => element.classList.remove('currentTab'));
+    document.querySelector("#portfolioButoonArtWork").classList.add('currentTab');
+});
+
+document.querySelector("#portfolioGallery").addEventListener('click',function(){
+    document.querySelectorAll("#portfolioGallery img").forEach(element => element.classList.remove('activeImg'));
+    event.target.classList.add('activeImg');
 });
